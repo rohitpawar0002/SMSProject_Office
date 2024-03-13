6ba6c1b1308a7dccb7d2622ff75f54f1d558a253
Rohit Pawar <rohitpawar0002@email.com>
2024-03-13 19:06:24 +0530

    error msg is not fetching from  template

--- src/app/View/sms-contant/sms/sms-contant.component.ts
+++ src/app/View/sms-contant/sms/sms-contant.component.ts
@@ -14,245 +14,233 @@ import { SidenavService } from 'src/app/Services/sidenav.service';
 export class SmsContantComponent {
 
 
-  smsForm!:FormGroup;
-display:any
-show:boolean=true;
-show2:boolean=true;
-mediaShow:boolean=true;
-submitted:boolean=false;
-updatecount:any;
-localcount:any;
-msglength:any;
-checkBoxClr:boolean=false;
-checkpoo:boolean=false;
-textlength:any;
-limit:any=0;
-validMobCount=0;
-invalidMobCount=0; 
-creditcount:any;   
-mobcount:any;
-pooArray:any=[];
+  smsForm!: FormGroup;
+  display: any
+  show: boolean = true;
+  show2: boolean = true;
+  mediaShow: boolean = true;
+  submitted: boolean = false;
+  updatecount: any;
+  localcount: any;
+  msglength: any;
+  checkBoxClr: boolean = false;
+  checkpoo: boolean = false;
+  textlength: any;
+  limit: any = 0;
+  validMobCount = 0;
+  invalidMobCount = 0;
+  creditcount: any;
+  mobcount: any;
+  pooArray: any = [];
 
-allselect:boolean=false;
+  poolMobile: any = '9730023006,\n7219550690';
+  testMobile: any = '9168266868'
 
+  allselect: boolean = false;
 
-fileoptionshow:boolean=false;
-groupOptionShow:boolean=true;
-mdediaVideoshow:boolean=true;
-mediaImgShow:boolean=false;
+  allnumbers: string = '';
+  // imageValue:any;
 
-
-constructor(private service:ServicesService,private formbuilder:FormBuilder,
-  private sidebarService:SidenavService,private reposrtService:ReposrtService,
-  private router:Router
-  ){}
-
-  
-showMain: boolean = true;
+  fileoptionshow: boolean = false;
+  groupOptionShow: boolean = true;
+  mdediaVideoshow: boolean = false;
+  mediaImgShow: boolean = true;
 
 
-ngOnInit(){
-  this.smsForm=this.formbuilder.group({
-    username:["demotr"],
-    password:["tr@1234"],
-    sender:['',Validators.required],
-    templateid:['',Validators.required],
-    mob:['',Validators.required],
-    msg:['',Validators.required],
-    coding:['1'],
-    clearChekBox:[false]
-
-  }); 
+  constructor(private service: ServicesService, private formbuilder: FormBuilder,
+    private sidebarService: SidenavService, private reposrtService: ReposrtService,
+    private router: Router
+  ) { }
 
 
-  this.sidebarService.showNav$.subscribe({
-    next:(res)=>{
-      this.showMain=res;
+  showMain: boolean = true;
+
+
+  ngOnInit() {
+    this.smsForm = this.formbuilder.group({
+      username: ["demotr"],
+      password: ["tr@1234"],
+      sender: ['', Validators.required],
+      templateid: ['', Validators.required],
+      mob: ['', Validators.required],
+      msg: ['', Validators.required],
+      coding: ['1'],
+      clearChekBox: [false]
+
+    });
+
+
+    this.sidebarService.showNav$.subscribe({
+      next: (res) => {
+        this.showMain = res;
+      }
+    })
+
+    this.limit = ''
+
+    this.smsForm.get('mob')!.valueChanges.subscribe(value => {
+      this.mobileNoCount(value)
+    })
+
+
+  }
+
+
+  mobileNoCount(value: string): void {
+
+
+
+    const mobileNumbers = value.split(',').map((number: any) => number.trim());
+
+    this.validMobCount = 0;
+    this.invalidMobCount = 0;
+
+    mobileNumbers.forEach((number: any) => {
+      if (number.length === 10 && /^\d+$/.test(number)) {
+        this.validMobCount++;
+      } else {
+        this.invalidMobCount++;
+      }
+    });
+
+    this.creditcount = this.validMobCount * this.limit;
+
+  }
+
+
+
+  // clear(){
+  //   debugger;
+  //   for(let i=0;i<this.smsForm.controls['mob'].value.length-1;i++){
+  //     if(this.smsForm.controls['mob'].value[i]=="\n"){
+  //       this.validMobCount--
+  //     }
+  //   }
+  // }
+
+  messageCount() {
+    // this.msglength=this.smsForm.controls['msg']
+    // console.log(this.msglength.value.length);
+    // this.textlength=this.msglength.value.length
+    this.smsForm.get('msg')!.valueChanges.subscribe(value => {
+      this.textlength = value.length;
+
+      if (this.textlength < 160) {
+        this.limit = 1;
+      }
+      else if (this.textlength % 160 == 0) {
+        let temp = this.textlength / 160
+        this.limit = Math.floor(temp);
+      }
+      else {
+        let temp = this.textlength / 160
+        this.limit = Math.floor(temp) + 1;
+      }
+      this.creditcount = this.validMobCount * this.limit;
+
+
+
+    })
+
+  }
+
+
+
+  openfileupload() {
+    this.fileoptionshow = true;
+    this.groupOptionShow = false
+
+  }
+
+  OngropOption() {
+    this.groupOptionShow = true
+    this.fileoptionshow = false;
+  }
+
+  OnmediaImg() {
+    this.mdediaVideoshow = false;
+    this.mediaImgShow = true;
+
+  }
+  Onmeidavideo() {
+    this.mdediaVideoshow = true;
+    this.mediaImgShow = false;
+  }
+
+
+  checkBoxFun(e: any) {
+    if (e.target.checked == true) {
+      this.checkBoxClr = true
+      this.smsForm.patchValue({
+        mob: ''
+      })
+      this.allselect = false
+
     }
-  })
-  
-   this.limit=''
-  
 
+  }
 
-  
-}
+  onAllselect(e: any) {
+    if (e.target.checked) {
+      this.allselect = true;
+      let selectedNumbers = this.poolMobile + ',\n' + this.testMobile;
+      this.smsForm.patchValue({
+        mob: selectedNumbers
+      });
+      this.mobileNoCount(selectedNumbers); // Assuming this function is used to count mobile numbers
+      this.checkBoxClr = false;
+    }
+    else {
+      this.allselect = false;
+      // Clear the phone numbers when the checkbox is unchecked
+      this.smsForm.patchValue({
+        mob: ''
+      });
+      this.invalidMobCount = 0;
+    }
+  }
 
-
-mobileNoCount(){
-  const mobileNumbers = this.smsForm.controls['mob'].value.split(',').map((number:any) => number.trim());
+  Onpoo(e: any) {
     
-  this.validMobCount = 0;
-  this.invalidMobCount = 0;
+    if (e.target.checked == true) {
+      let currentNumbers = this.smsForm.get('mob')?.value;
 
-  mobileNumbers.forEach((number:any) => {
-    if (number.length === 10 && /^\d+$/.test(number)) {
-      this.validMobCount++;
-    } else {
-      this.invalidMobCount++;
+      if (!currentNumbers.includes(this.poolMobile)) {
+        currentNumbers += ',\n' + this.poolMobile;
+        this.smsForm.patchValue({
+          mob: currentNumbers
+        });
+        this.mobileNoCount(currentNumbers);
+        this.checkBoxClr = false;
+      }
     }
-  });
-
-  this.creditcount=this.validMobCount*this.limit;
-
-
-  
-//  this.mobcount=this.smsForm.controls['mob'].value
-//  let splitcount=this.mobcount.split('\n').map((number:any)=>number.trim());
-// // const mob = splitcount.join(',');
-
-// console.log('splicoiunt',splitcount);
-
-// let fixmobilecount=new Set<string>(splitcount)
-// const tempArray:any=[]
-// const mobileArray=Array.from(fixmobilecount);
-// let result = mobileArray.filter(
-//   (element,index,mobileArray)=>{
-
-//     if(element)
+    else {
+      let replaceVar = this.smsForm.controls['mob'].value.replace(this.poolMobile, '');
+      this.smsForm.patchValue({
+        mob: replaceVar
+      });
+    }
    
-//       tempArray.push(element)
-      
-    
-//     return ;
-//   }
-// );
-
-// const lastmobilenumber=tempArray[tempArray.length -1];
-
-
-
-
-// if(this.validMobileNumbers(lastmobilenumber)){
-//   this.validMobCount++
-// }
-// else{
-//   this.invalidMobCount++
-//   this.smsForm.patchValue({
-//     mob:''
-//   })
-// }
-// }
-
-// validMobileNumbers(lastumber:any):boolean{
-//   if(lastumber.length==10){
-//     return true;
-//   }
-//   else{
-//     return false;
-//   }
-}
-
-// clear(){
-//   debugger;
-//   for(let i=0;i<this.smsForm.controls['mob'].value.length-1;i++){
-//     if(this.smsForm.controls['mob'].value[i]=="\n"){
-//       this.validMobCount--
-//     }
-//   }
-// }
-
-messageCount(){
-  debugger;
-  // this.msglength=this.smsForm.controls['msg']
-  // console.log(this.msglength.value.length);
-  // this.textlength=this.msglength.value.length
-  this.smsForm.get('msg')!.valueChanges.subscribe(value=>{
-    this.textlength=value.length;
-
-    if(this.textlength<160)
-    {
-      this.limit=1;
-    }
-    else if(this.textlength %160==0){
-      let temp=this.textlength/160
-      this.limit=Math.floor(temp);
-    }
-    else{
-      let temp=this.textlength/160
-      this.limit=Math.floor(temp)+1;
-    }
-    this.creditcount=this.validMobCount*this.limit;
-
-  
-      
-  })
-  
   }
 
-
-
-  openfileupload(){
-    this.fileoptionshow=true;
-    this.groupOptionShow=false
-
-  }
-
-  OngropOption(){
-    this.groupOptionShow=true
-    this.fileoptionshow=false;
-  }
-
-  OnmediaImg(){
-    this.mdediaVideoshow=false;
-    this.mediaImgShow=true;
-
-  }
-  Onmeidavideo(){
-    this.mdediaVideoshow=true;
-    this.mediaImgShow=false;
-  }
-
-
-checkBoxFun(){
-  this.checkBoxClr=true
-  this.smsForm.patchValue({
-    mob:''
-  })
-  }
-
-  onAllselect(e:any){
-    if(e.target.checked==true){
-      this.allselect=true
-      this.smsForm.patchValue({
-        mob:['9730023006,\n7219550690,\n9168266868']
-      })
-    }
-    else if(e.target.checked==false){
-      this.allselect=false
-      this.smsForm.patchValue({
-        mob:''
-      })
-    }
-  }
-
-  Onpoo(e:any){
-    // this.pooArray=;
-
-    if(e.target.checked==true){
-      this.smsForm.patchValue({
-        mob:['9730023006,\n7219550690']
-      })
-    }
-    else if(e.target.checked==false){
-      this.smsForm.patchValue({
-        mob:''
-      })
-    }
-  }
-
-    test(e:any){
-      if(e.target.checked==true){
+  test(e: any) {
+    if (e.target.checked) {
+      let currentNumbers = this.smsForm.get('mob')?.value || ''; // Ensure the value is not null
+      if (!currentNumbers.includes(this.testMobile)) {
+        currentNumbers += ',\n' + this.testMobile;
         this.smsForm.patchValue({
-          mob:['9168266868']
-        })
+          mob: currentNumbers
+        });
+        this.mobileNoCount(currentNumbers);
+        this.checkBoxClr = false;
       }
-      else if(e.target.checked==false){
-        this.smsForm.patchValue({
-          mob:''
-        })
-      }
+    } else {
+      let replaceVar = this.smsForm.controls['mob'].value.replace(this.testMobile, '');
+      this.smsForm.patchValue({
+        mob: replaceVar
+      });
     }
+  }
 
 
   //  if(this.checkpoo=true){
@@ -266,83 +254,95 @@ checkBoxFun(){
   //   })
   //  }
 
- 
 
 
+  // onOptionImageSelected(e: any) {
+  //   debugger
+  //   this.selectedImage = `http://localhost:4200/assets/${e.target.value}`
 
-senderArray=['NUEVAS','TRKZIA']
+  //   let imageValue = e.target.value
+  //   console.log('valueeee', this.selectedImage);
 
-templateArray=[
-  {key:'Your My SMS verification Code id 1234. Do not share this code with others Team Nuevas',value:'1707161891201501738'},
-  {key:'Dear User your OTP is  Kindly use OTP to validate your Registration. Team Trackzia',value:'1707161855199873979'},
-  {key:'Dear 1234 , Your Complaint with Complaint Id: 1234 has Been Resolve Kindly Share OTP, The OTP is 1234 \n From Nuevas',value:'1707161899992775140'}
-]
+  // }
 
-changevalue(data:any){
-  // console.log(data.target.value);
-  const key=this.templateArray.find((t)=>t.value==data.target.value)
-    
-  
+
+  senderArray = ['NUEVAS', 'TRKZIA']
+
+  templateArray = [
+    { key: `Your My SMS verification Code id 1234. Do not share this code with others Team Nuevas',value:'1707161891201501738` },
+    { key: 'Dear User your OTP is  Kindly use OTP to validate your Registration. Team Trackzia', value: '1707161855199873979' },
+    { key: 'Dear 1234 , Your Complaint with Complaint Id: 1234 has Been Resolve Kindly Share OTP, The OTP is 1234 \n From Nuevas', value: '1707161899992775140' }
+  ]
+
+  changevalue(data: any) {
+    debugger;
+
+    // console.log(data.target.value);
+    const key = this.templateArray.find((t) => t.value == data.target.value)
+
+
     this.smsForm.patchValue({
       // msg:'Your My SMS verification Code id . Do not share this code with others Team Nuevas',
-      msg:key?.key      
+      msg: key?.key
       // msg:'Dear User your OTP is {#var#} Kindly use OTP to validate your Registration. Team Trackzia'
       // msg:'Dear {#var#} , Your Complaint with Complaint Id: {#var#} has Been Resolve Kindly Share OTP, The OTP is {#var#} \n From Nuevas'
     })
-    debugger;
-    this.textlength=key?.key.length;
-    this.limit=1
+    this.textlength = key?.key.length;
+    this.limit = 1
 
     this.messageCount()
   }
 
 
-onSubmit(){
- 
+  onSubmit() {
 
-  this.submitted=true;
-  if(this.smsForm.invalid){
-    alert('Please check all fileds')
-    return;
-  }
-    this.service.getsms(this.smsForm.value).subscribe((res:any)=>{
-      if(res.Success==true){      
+    this.submitted = true;
+    if (this.smsForm.invalid) {
+      alert('Please check all fileds')
+      return;
+    }
+    this.service.getsms(this.smsForm.value).subscribe((res: any) => {
+      if (res.Success == true) {
         alert("Message send successfully!")
-        console.log(this.smsForm.value); 
+        console.log(this.smsForm.value);
         console.log(res);
-        this.service.userName=this.smsForm.value.username;
-        localStorage.setItem('count',JSON.stringify( this.service.userName));
-        this.reposrtService.ResArray=res;
+        this.service.userName = this.smsForm.value.username;
+        localStorage.setItem('count', JSON.stringify(this.service.userName));
+        this.reposrtService.ResArray = res;
         this.reposrtService.postReportAPI(res).subscribe({
-          next:(res:any)=>{
-            this.router.navigate(['report'])   
-            console.log(res);    
+          next: (res: any) => {
+            this.router.navigate(['report'])
+            console.log(res);
 
           }
         })
         // localStorage.setItem('report',JSON.stringify(this.reposrtService.ResArray))
       }
-      else{
+      else {
         alert('Somwthing went wrong');
         console.log(res);
-        
+
       }
-                 
+
     });
   }
 
 
 
-  showOption(){
-    this.show=!this.show          
+  showOption() {
+    this.show = !this.show
   }
 
-  showOptionMedia(){
-    this.mediaShow=!this.mediaShow
+  showOptionMedia() {
+    this.mediaShow = !this.mediaShow
   }
-  showOption2(){
-    this.show2=!this.show2          
+  showOption2() {
+    this.show2 = !this.show2
   }
+
+  selectedImage: any = ''
+
+
 }
 
 
